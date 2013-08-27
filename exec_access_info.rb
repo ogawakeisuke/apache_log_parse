d6ad3d258663705898b22bdb233baf23e11f368b
ogawakeisuke <ogawa@uniba.jp>
2013-08-27 13:17:14 +0900

    完成

--- exec_access_info.rb
+++ exec_access_info.rb
@@ -17,24 +17,6 @@ def access_info_create(entry)
 end
 
 
-def update_pv_and_uu(entry)
-  entry.update(:pv => (entry.pv) + 1, :uu => (entry.pv) + 1)
-end
-
-def update_pv(entry)
-  entry.update(:pv => (entry.pv) + 1)
-end
-
-
-
-# def reprace_process(entry)
-#   ai = AccessInfo.where(:url => entry.request[:path])
-#   if ai.first && 
-
-#   end
-# end
-
-
 # DOWNLOADS_ROOT = "/Users/ogawa/Downloads/"
 # Dir.glob("#{DOWNLOADS_ROOT}log/access_lo*")
 
@@ -49,17 +31,24 @@ Dir.glob("access_lo*").sort.each_with_index do |f, i|
   # datetimeが同じものでgroup
   entries = entries.delete_if { |e| e.request[:path] =~ /wordpress/ }.group_by {|entry| entry.datetime.to_date }
 
-  entries.each do |date_key, entry_collection|
-    entries[date_key] = entry_collection.group_by {|e| e.request[:path] }
+  entries.each do |date_key, date_entries|
+    entries[date_key] = date_entries.group_by {|e| e.request[:path] }
   end
-  pp entries
 
+  #
+  # DBに書き込み
+  #
+  entries.each do |date_key, date_enies|
+    date_enies.each do |url_key, url_entries|
+      pv = url_entries.count
+      uu = url_entries.collect {|entry| entry.ip_address }.uniq.count
+      AccessInfo.create(
+        :url => url_key,
+        :pv => pv,
+        :uu => uu,
+        :date_at => date_key
+        )
+    end
+  end
+  
 end
-
-
-
-# File.open("out.txt", "w") do |io|
-#   request_hashes.each_pair do |k,v|
-#     io.puts  "#{k} = #{v}"
-#   end
-# end
