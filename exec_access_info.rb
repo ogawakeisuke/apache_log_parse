1d4eaaac53d8870ecf4fedcc9897c378338537e2
ogawakeisuke <ogawa@uniba.jp>
2013-08-28 15:23:39 +0900

    不必要なものを削除

--- exec_access_info.rb
+++ exec_access_info.rb
@@ -1,36 +1,25 @@
 require "./apache_log"
 require "./access_info"
-require "pp"
-
-request_hashes = Hash.new
-
-
-
-
-def access_info_create(entry)
-  AccessInfo.create(
-    :url => entry.request[:path],
-    :pv => 1,
-    :uu => 1,
-    :date_at => entry.datetime
-  )
-end
-
 
 DOWNLOADS_ROOT = "/Users/ogawa/Downloads/"
+
 Dir.glob("#{DOWNLOADS_ROOT}log/access_lo*").sort.each_with_index do |f, i|
   entries = []
+
   File.foreach(f) do |line|
     entries << ApacheLog.parse(line.chomp)
   end
 
-  # TODO refactaring
-  # wordpressなどの単語を除外
-  # datetimeが同じものでgroup
-  entries = entries.delete_if { |e| e.request[:path] =~ /wordpress/ }.group_by {|entry| entry.datetime.to_date }
+  #
+  # wordpressなどの単語を除外し、datetimeが同じものの集合を作る
+  #
+  entries = entries.delete_if { |e| e.request[:path] =~ /wordpress/ }.group_by {　|entry| entry.datetime.to_date }
 
+  #
+  # 同じdatetimeの集合の中でさらに、url(request[:path])が同じ集合を作る(ネストする)
+  #
   entries.each do |date_key, date_entries|
-    entries[date_key] = date_entries.group_by {|e| e.request[:path] }
+    entries[date_key] = date_entries.group_by {　|e| e.request[:path] }
   end
 
   #
@@ -40,12 +29,13 @@ Dir.glob("#{DOWNLOADS_ROOT}log/access_lo*").sort.each_with_index do |f, i|
     date_enies.each do |url_key, url_entries|
       pv = url_entries.count
       uu = url_entries.collect {|entry| entry.ip_address }.uniq.count
+      
       AccessInfo.create(
         :url => url_key,
         :pv => pv,
         :uu => uu,
         :date_at => date_key
-        )
+      )
     end
   end
   
